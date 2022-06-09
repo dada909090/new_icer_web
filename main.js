4c5eec6aede1e608b0c44a7926df0830162c7152
tangperry <tytytyt6372@gmail.com>
2022-06-09 20:21:13 +0800

    new file

---
+++ main.js
@@ -0,0 +1,10 @@
+const screen_hight = $(window).height();
+$("#header").height(screen_hight);
+
+let display = 0;
+for (let i = 0; i < 1000; i += 20) {
+    setTimeout(() => {
+        $("body").attr("style", `background-image: linear-gradient(90deg, rgba(99, 237, 255, 1) 0%, rgba(46, 144, 243, 1) ${display*100}%, rgba(99, 237, 255, 1) 110%);`);
+        display += 0.01;
+    },i);
+}
